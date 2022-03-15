1b1659fe5f694a1ab5335ba4ee53b17a90f0fa17
Zan Javed <zjaved101@gmail.com>
2022-03-15 16:27:21 -0700

    login activity

---
+++ frontend/app/src/main/java/com/example/frontend/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        TextView view = findViewById(R.id.loginTitle);
+        EditText email = findViewById(R.id.editTextEmail);
+        EditText password = findViewById(R.id.editTextPassword);
+
+        Call<User> call = RetrofitClient.getInstance().getMyApi().postLogin(new Login(email.getText().toString(), password.getText().toString()));
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                System.out.println("INSIDE RESPONSE");
+                view.setText("inside response");
+                if(response.isSuccessful() && response.body() != null) {
+                    User login = response.body();
+                    System.out.println(login.getFirstName());
+                    view.setText(String.valueOf(login.getId()));
+                } else {
+                    System.out.println("Response broke");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.e("onFailure error", t.getMessage());
+                Toast.makeText(getApplicationContext(), "An error has occurred", Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
